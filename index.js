8d73901601f357738ae4cffd7ed214cab412b4ac
sonugupta4636 <sonugupta4636@gmail.com>
2017-12-17 22:18:15 +0530

    Handling for 'Broadcast' messages.  * Added logic for showing '## is typing message..' onto all other clients except the sender.  * Added logic for handling 'out_of_focus'[blur] events.

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// @author: Sonu Gupta
+// @date: 17-dec-2017
+// @Application: Chat room application using websockets.
+
+
 // This will act as a 'SERVER'
 
 var express = require('express');
@@ -10,24 +15,35 @@ var server = app.listen(4000, function(){
 })
 
 //static files
-// This means, express is gona say its gona look for 'public' folder and if it finds any resources in 'public' folder, its gona send to browser.
+// This means, express is gona look for 'public' folder and if it finds any resources[.css, .html etc.] in 'public' folder, its gona send to browser.
 app.use(express.static('public'));
 
 //Socket Setup
 var io = socket(server);
 
 // This is listening for event 'connection' [when browser connects to port 4000]
+
 // callback functions parameeter 'socket' is the instance of the socket that got connected.
 // Note: To work correctly, you also need to use the same 'socket.io' on client [index.html] :)
 
 io.on('connection', function(socket){
     console.log('successfully made socket connection', socket.id);
 
-    // Note: Here, we are listening for 'chat' event on 'SOCKET' instance
+    //Listener 1: 'chat'
     socket.on('chat', function(data){
-        console.log('chat message received')
+        console.log('chat_event_received')
+        io.sockets.emit('chat', data);                 // Notice: io.broadcast.emit() -----> Send to all
+    });
 
-        //Followint 'io.sockets' will emit the message to all the connected clients.
-        io.sockets.emit('chat', data);
+    //Listener 2: 'typing'
+    socket.on('typing', function(data){
+        console.log('typing_event_received');
+        socket.broadcast.emit('typing', data)           // Notice: socket.broadcast.emit()  -----> Send to all except itself
+    });
+
+    //Listener 2: 'typing'
+    socket.on('outoffocus', function(data){
+        console.log('outoffocus_event_received');
+        socket.broadcast.emit('outoffocus', data)        // Notice: socket.broadcast.emit() -----> Send to all except itself
     });
 });
--- public/chat.js
+++ public/chat.js
@@ -1,3 +1,7 @@
+// @author: Sonu Gupta
+// @date: 17-dec-2017
+// @Application: Chat room application using websockets.
+
 // Make connection
 
 // You might be worried abt 'io' variable. :D
@@ -11,10 +15,13 @@ var message = document.getElementById('message');
 var btn = document.getElementById('send');
 var output = document.getElementById('output');
 var handle = document.getElementById('handle');
+var feedback = document.getElementById('feedback');
 
-// Emit events
-
-// 'addEventListener' is the boiler plate event provided by javascript
+//========================================================================================================================================================
+// Emit events:[To server]
+//
+//=========================================================================================================================================================
+    // "click" Note: 'addEventListener' is the boiler plate event provided by javascript
 btn.addEventListener('click', function(){
     //First parameter is any 'nameOfMessage' and second parameter is the object containing 'Actual data'
     // Notice that at server side, we are going to listen onto the same 'nameOfMessage'
@@ -24,8 +31,38 @@ btn.addEventListener('click', function(){
     });
 });
 
-//NOte: SOCKET: Listening for event 'chat' that was emitted by server and to display the same onto browser screen.
+    // "keypress" Event for broadcasting 'typing' Message
+    // Note: Whenever message is typed by indivisual, 'keypress event' is fired. Here, we are catching the event and emitting to server.
+    // Thus, server on its end will listen for this 'typing' event and will broadcast the same.
+message.addEventListener('keypress', function(){
+    socket.emit('typing', handle.value);
+});
 
+    // "blur" Event for clearing 'typing' Message
+    // Note: Whenever someone stops writing message, '### is typing a message..' should no longer be displayed
+message.addEventListener('blur', function(){
+    socket.emit('outoffocus', handle.value);
+});
+
+//========================================================================================================================================================
+//Listen for events: [from server]
+//
+//========================================================================================================================================================
+
+    //Note: SOCKET: Listening for event 'chat' that was emitted by server and to display the same onto browser screen.
 socket.on('chat', function(data){
+    feedback.innerHTML = "";
     output.innerHTML += '<p><strong>' + data.handle + ':</strong>' + data.message + '</p>';
+});
+
+    //Note: SOCKET: Listening for event 'typing' that was emitted by server and to display the same onto browser screen.
+socket.on('typing', function(data){
+    console.log('typing_event_fired');
+    feedback.innerHTML = '<p><em>'+ data + ' is typing a message...</em></p>';
+});
+
+    //Note: SOCKET: Listening for event 'outoffocus' that was emitted by server and to clear the previous 'typing' message.
+socket.on('outoffocus', function(data){
+    console.log('out_of_focus');
+    feedback.innerHTML = "";
 });
